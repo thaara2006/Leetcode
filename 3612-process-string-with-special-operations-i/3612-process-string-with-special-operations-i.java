4fd8e303b73885d1f280e8be3cfea8b588182144
thaara2006 <thaaranyaashree@gmail.com>
2026-06-16 14:27:39 +0530

    Time: 3 ms (100%), Space: 55 MB (80.88%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,28 +1,44 @@
 class Solution {
     public String processStr(String s) {
-        String r="";
+        StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(Character.isLetter(s.charAt(i))) r+=s.charAt(i);
-            else if(s.charAt(i)=='*'){
-                if(r.length()>0){   
-                    StringBuilder sb=new StringBuilder(r);
-                    sb.deleteCharAt(r.length()-1);
-                    r=sb.toString();
+            char c=s.charAt(i);
+            if(c=='*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
                 }
             }
-            else if(s.charAt(i)=='#'){
-                if(r.length()>0){
-                    //while()
-                    r=r+r;
-                }
-                continue;
+            else if(c=='#'){
+                sb.append(sb);
             }
-            else if(s.charAt(i)=='%'){
-                StringBuilder sb=new StringBuilder(r);
+            else if(c=='%'){
                 sb.reverse();
-                r=sb.toString();
             }
+            else{
+                sb.append(c);
+            }
+            // if(Character.isLetter(s.charAt(i))) 
+            //     r+=s.charAt(i);
+            // else if(s.charAt(i)=='*'){
+            //     if(r.length()>0){   
+            //         StringBuilder sb=new StringBuilder(r);
+            //         sb.deleteCharAt(r.length()-1);
+            //         r=sb.toString();
+            //     }
+            // }
+            // else if(s.charAt(i)=='#'){
+            //     if(r.length()>0){
+            //         //while()
+            //         r=r+r;
+            //     }
+            //     continue;
+            // }
+            // else if(s.charAt(i)=='%'){
+            //     StringBuilder sb=new StringBuilder(r);
+            //     sb.reverse();
+            //     r=sb.toString();
+            // }
         }
-        return r;
+        return sb.toString();
     }
 }
