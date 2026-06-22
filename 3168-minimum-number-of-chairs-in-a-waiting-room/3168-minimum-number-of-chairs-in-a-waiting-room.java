b0da0f250a2be5a13fb783d4eb38eb44dc95a610
thaara2006 <thaaranyaashree@gmail.com>
2026-06-22 15:20:07 +0530

    Time: 1 ms (100%), Space: 43.3 MB (92.36%) - LeetHub

--- 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.java
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.java
@@ -3,15 +3,13 @@ class Solution {
         int c=0,p=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='E') {
-                if(c<p){
-                    c++;
-                }
-                    p++;
+                if(c<p) c++; // initially 0<0(c<p), so one chair is always less, then chair increments only when chair less than person
+                p++; // person increments whenever person enters
             }
             else{
-                p--;
+                p--; // person decrements whenever person leaves
             }
         }
-        return c+1;
+        return c+1; // due to initial condition, one chair adds to the resulting chair(c)
     }
 }
