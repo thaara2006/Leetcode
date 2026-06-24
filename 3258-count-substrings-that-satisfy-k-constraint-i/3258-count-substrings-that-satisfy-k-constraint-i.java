68d68faa683761333bc6d4b99eb1acb4497c725f
thaara2006 <thaaranyaashree@gmail.com>
2026-06-24 15:15:08 +0530

    Time: 26 ms (6.75%), Space: 47 MB (5%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
@@ -2,7 +2,7 @@ class Solution {
     public int countKConstraintSubstrings(String s, int k) {
         int c=0,i=1;
         while(i<=s.length()){
-            for(int j=0;j+i<=s.length();j++){
+            for(int j=0;j<=s.length()-i;j++){
                 //System.out.println("inner loop");
                 String b=s.substring(j,j+i);
                //System.out.println(b);
