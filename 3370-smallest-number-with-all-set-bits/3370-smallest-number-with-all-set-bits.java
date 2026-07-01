758fcbefb1edb7b699544fe348053d85926375ba
thaara2006 <thaaranyaashree@gmail.com>
2026-07-01 12:01:47 +0530

    Time: 1 ms (48.25%), Space: 42.6 MB (21.27%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,24 +1,27 @@
 class Solution {
     public int smallestNumber(int n) {
-        for(int i=n;i<=2000;i++){
-            String b=toBin(i);
-            boolean fb=true;
-            for(int j=0;j<b.length();j++){
-                if(b.charAt(j)=='0') {
-                    fb=false;
-                    break;
-                }
-            }
-            if(fb) return i;
-        }
-        return 0;
+        String b=Integer.toBinaryString(n);
+        return (int)Math.pow(2,b.length())-1;
     }
-    private String toBin(int n){
-        StringBuilder s=new StringBuilder();
-        while(n>0){
-            s.append(n%2);
-            n/=2;
-        }
-        return s.reverse().toString();
-    }
+    //     for(int i=n;i<=2000;i++){
+    //         String b=toBin(i);
+    //         boolean fb=true;
+    //         for(int j=0;j<b.length();j++){
+    //             if(b.charAt(j)=='0') {
+    //                 fb=false;
+    //                 break;
+    //             }
+    //         }
+    //         if(fb) return i;
+    //     }
+    //     return 0;
+    // }
+    // private String toBin(int n){
+    //     StringBuilder s=new StringBuilder();
+    //     while(n>0){
+    //         s.append(n%2);
+    //         n/=2;
+    //     }
+    //     return s.reverse().toString();
+    // }
 }
