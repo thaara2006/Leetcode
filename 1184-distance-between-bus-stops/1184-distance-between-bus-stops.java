6eebae4cfa3ee3d89f3d23d57500a76d010609fc
thaara2006 <thaaranyaashree@gmail.com>
2026-08-08 12:42:43 +0530

    Time: 4 ms (2.97%), Space: 44.5 MB (8.9%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.java
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.java
@@ -1,31 +1,25 @@
 class Solution {
     public int distanceBetweenBusStops(int[] distance, int start, int destination) {
         int n=distance.length;
-        int s1=0, s2=0;
+        int rev=0, frw=0;
+        int t=0;
+        for(int i=0;i<n;i++){
+            t+=distance[i];
+        }
         if(destination<start){
             for(int i=destination;i<start;i++){
-                s1+=distance[i];
+                rev+=distance[i];
             }
-            for(int i=0;i<destination;i++){
-                s2+=distance[i];
-            }
-            for(int i=start;i<n;i++){
-                s2+=distance[i];
-            }
+            frw=t-rev;
         }
         else{
             for(int i=start;i<destination;i++){
-                s1+=distance[i];
+                frw+=distance[i];
             }
-            for(int i=0;i<start;i++){
-                s2+=distance[i];
-            }
-            for(int i=destination;i<n;i++){
-                s2+=distance[i];
-            }
+            rev=t-frw;
         }
-        System.out.println(s1+" "+s2);
-        return Math.min(s1, s2);
-        //return s1;
+        
+        System.out.println(frw+" "+rev);
+        return Math.min(frw, rev);
     }
 }
